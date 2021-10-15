3327e7a025f2b6d5cfef7f34014eba53e7297cb1
blik3230 <3blik230@gmail.com>
2021-10-15 23:57:55 +0300

    Add custom fonts and variables of css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import React, { Fragment } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <meta name="description" content="Курс изучения nextjs из udemy" />
+        <link rel="icon" href={ '/favicon.ico' } />
+      </Head>
+
+      <Component { ...pageProps } />
+    </Fragment>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document';
+import React from 'react';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -15,7 +16,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={'ru'}>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
